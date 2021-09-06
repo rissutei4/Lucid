b1f6acedd8fd6625f3ca768fd458e35fb69c41dc
rissutei4 <onmadxck@gmail.com>
2021-09-06 23:17:05 +0300

    almost ready to use website, still needs some work to be done

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,33 @@ $(document).ready(function(){
         }
     });
 });
+
+$(document).ready(function () {
+    //sticky navbar
+    window.onscroll = () => {
+        var header = document.querySelector('header');
+
+        if (window.pageYOffset > 0) {
+            header.classList.add("sticky")
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+    //scroll with appearing items
+    window.addEventListener('scroll', reveal);
+
+    function reveal() {
+        var reveals = document.querySelectorAll('.reveal');
+
+        for (var i = 0; i < reveals.length; i++) {
+
+            var windowheight = window.innerHeight;
+            var revealtop = reveals[i].getBoundingClientRect().top;
+            var revealpoint = 150;
+
+            if (revealtop < windowheight - revealpoint) {
+                reveals[i].classList.add('active');
+            }
+        }
+    }
+});
